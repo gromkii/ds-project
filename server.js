9c8c2a5c8ad26ed7c77039d3499b341748127bf4
Dax <limeside@gmail.com>
2016-07-14 10:20:41 -0500

    Add server file, established connection. :rabbit:

---
+++ server.js
@@ -0,0 +1,24 @@
+var express        = require('express'),
+    app            = express(),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('method-override');
+
+require('locus');
+
+// === Middleware === //
+app.use(bodyParser.json())
+  .use(bodyParser.urlencoded({extend:false}))
+  .use(methodOverride('_method'))
+  .use(express.static('public'));
+
+// === Routes === //
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/views/index.html')
+});
+
+// === Server === //
+
+app.listen(3000, (req, res) => {
+  console.log('Server is listening.');
+});
