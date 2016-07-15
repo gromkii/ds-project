dbd92aef4921e37767382978c34169b1df872573
Dax <limeside@gmail.com>
2016-07-15 09:34:13 -0500

    Add google map, not quite working yet. :rabbit:

---
+++ public/javascript/map.js
@@ -0,0 +1,11 @@
+// === Google Map, refactor if it works. === //
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
+
+document.addEventListener('load',initMap);
