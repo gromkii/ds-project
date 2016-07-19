068d1f5c483640c69bb00f77f7cfb73d68fa2f06
Dax <limeside@gmail.com>
2016-07-19 11:51:42 -0500

    Refactor autocomplete. Need to add styling for it. :rabbit:

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -46,35 +46,7 @@ app.controller("SearchFormController", function(){
   var store = this;
   this.autoFill = [];
 
-  $('#beer1').autocomplete({
-    source: function(req, res){
-      $.getJSON({
-        url:`http://dax-cors-anywhere.herokuapp.com/http://ec2-54-235-57-99.compute-1.amazonaws.com:5000/v1.0.0/autocomplete?q=${req.term}`,
-        function(data){
-          res(data);
-        }
-      }).then(function(results){
-        store.autoFill.beers = results.response;
-        console.log(store.autoFill.beers);
-      })
-    },
-    minLength:3
-  });
-  $('#beer2').autocomplete({
-    source: function(req, res){
-      $.getJSON({
-        url:`http://dax-cors-anywhere.herokuapp.com/http://ec2-54-235-57-99.compute-1.amazonaws.com:5000/v1.0.0/autocomplete?q=${req.term}`,
-        function(data){
-          res(data);
-        }
-      }).then(function(results){
-        store.autoFill.beers = results.response;
-        console.log(store.autoFill.beers);
-      })
-    },
-    minLength:3
-  });
-  $('#beer3').autocomplete({
+  $('#beer1,#beer2,#beer3').autocomplete({
     source: function(req, res){
       $.getJSON({
         url:`http://dax-cors-anywhere.herokuapp.com/http://ec2-54-235-57-99.compute-1.amazonaws.com:5000/v1.0.0/autocomplete?q=${req.term}`,
